45c81f2c39c3c69467db80bc61482d814866f145
Matt Neary <neary.matt@gmail.com>
2014-07-29 20:33:11 -0400

    simple single-character parser

--- swiper/swiper.swift
+++ swiper/swiper.swift
@@ -1,3 +1,16 @@
+extension String {
+  subscript (i: Int) -> String {
+    return String(Array(self)[i])
+  }
+  subscript (r: Range<Int>) -> String {
+    get {
+      let startIndex = advance(self.startIndex, r.startIndex)
+      let endIndex = advance(startIndex, r.endIndex - r.startIndex)
+      return self[Range(start: startIndex, end: endIndex)]
+    }
+  }
+}
+
 public enum SResult {
   case Success(String, String)
   case Failure
@@ -12,5 +25,17 @@ public let unitParser : Parser = {
   (s : String) -> SResult
   in
   return .Success("", s)
+}
+
+func charParser(c : String) -> Parser {
+  return {
+    (s : String) -> SResult
+    in
+    if countElements(s) > 0 && s[0] == c {
+      return .Success(c, s[1..<countElements(s)])
+    } else {
+      return .Failure
+    }
+  }
 }
 
--- swiperTests/tests.swift
+++ swiperTests/tests.swift
@@ -11,10 +11,36 @@ class ParserTests {
       default: return true
     }
   }
+  func testCharParser() -> Bool {
+    let parser = charParser("a")
+    let match = {
+      () -> Bool in
+      switch parser("a") {
+	case let .Success(a,rest): return a == "a" && rest == ""
+	default: return false
+      }
+    }
+    let submatch = {
+      () -> Bool in
+      switch parser("ab") {
+	case let .Success(a,rest): return a == "a" && rest == "b"
+	default: return false
+      }
+    }
+    let failure = {
+      () -> Bool in
+      switch parser("c") {
+	case let .Success(_,_): return false
+	default: return true
+      }
+    }
+    return match() && submatch() && failure()
+  }
   func runTests() {
     let tests = [
       testUnitParser,
-      testZeroParser
+      testZeroParser,
+      testCharParser
     ]
     for test in tests {
       print(test() ? "." : "F")
